5fe89656a3349917996561f3ce229f7125f61ef0
otoshuki <guining.pertin.iitg@gmail.com>
2020-05-13 19:02:21 +0530

    Improved config_space and motion_planner nodes

--- ros_src/motion_planner_node.py
+++ ros_src/motion_planner_node.py
@@ -1,40 +1,75 @@
-#Guining Pertin - 14-05-20
-import sys
-import os
+#Guining Pertin
+#Motion planner node - 14-05-20
+
+#Import libraries
+#ROS
 import rospy
-import matplotlib.pyplot as plt
-import numpy as np
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from std_msgs.msg import String
+#Others
+import sys
+import os
+import numpy as np
 
+#MoPAT alogirithm files
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
-from mopat_astar import Astar
+from algorithms.mopat_astar import Astar
 
+#Global variables
+got_config_space = False
+index = 0
+screen_size = None
+start = (0,0)
+goal = (99,99)
+
+#CvBridge object required for conversion
 bridge = CvBridge()
-done = False
 
-def callback(data):
-    global done
-    if not done:
-        conv_image = bridge.imgmsg_to_cv2(data, desired_encoding = "passthrough")
-        bool_mat = conv_image.astype(bool)
-        print("Got Config Space")
-        plt.matshow(bool_mat)
-        plt.show(block=False)
-        plt.pause(0.5)
-        astar_obj = Astar(0, bool_mat, 0, 0, 100, 100)
-        py, px = astar_obj.find_best_route(100 - 0,0, 100 - 99, 99)
-        plt.plot(px, py, "red")
-        plt.show()
-        done = True
+def config_space_cb(data):
+    '''
+    Get config_space data
+    Arguments:
+        data    :   ROS sensor_msgs/Image
+    '''
+    global got_config_space
+    global config_space
+    global screen_size
+    if not got_config_space:
+        config_space = bridge.imgmsg_to_cv2(data, desired_encoding = "passthrough")
+        config_space = config_space.astype(bool)
+        print("LOG: Got configuration space")
+        screen_size = config_space.shape
+        got_config_space = True
 
-def main():
-    rospy.init_node("Motion_planner", anonymous = True)
-    subs = rospy.Subscriber("mopat/config_space", Image, callback)
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting Down")
+def motion_planner_node():
+    '''
+    Create notion planner node
+    '''
+    global config_space
+    #Initialize node
+    rospy.init_node("motion_planner_node", anonymous = True)
+    #Subscribe to config_space data - Image
+    rospy.Subscriber("mopat/config_space", Image, config_space_cb)
+    #Publish motion plan
+    pub = rospy.Publisher("mopat/motion_plan", String, queue_size=5)
+    #Set rate
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        #Don't generate motion plan until config space is found
+        if got_config_space:
+            astar_obj = Astar(index, config_space,
+                            0, 0, screen_size[1], screen_size[0])
+            plan_y, plan_x = astar_obj.find_best_route(screen_size[0]-start[1],
+                                                       start[0],
+                                                       screen_size[0]-goal[1],
+                                                       goal[0])
+            motion_plan = (plan_x, plan_y)
+            pub.publish("Done")
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        motion_planner_node()
+    except rospy.ROSInterruptException:
+        pass
