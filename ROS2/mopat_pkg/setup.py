5d3fd4b0a1540bb8f50816e345728b844dad78b2
Guining Pertin <guining.pertin.iitg@gmail.com>
2020-07-20 19:27:18 +0530

    Added current progress in ros2 package build

---
+++ ROS2/mopat_pkg/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'mopat_pkg'
+
+setup(
+    name=package_name,
+    version='5.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='otoshuki',
+    maintainer_email='guining.pertin.iitg@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            # 'simulator_node = mopat_pkg.simulator_node:main',
+            'camera_node = mopat_pkg.camera_node:main',
+        ],
+    },
+)
