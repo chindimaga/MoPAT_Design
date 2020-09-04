35426aaaa185c317149843973d08f98c1a336c61
Guining Pertin <guining.pertin.iitg@gmail.com>
2020-09-04 16:35:07 +0530

    Added OTA support for nodemcu

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 import socket
 import time
 
-HOST = '172.20.10.3'  # Standard loopback interface address (localhost)
+HOST = '192.168.43.135'  # Standard loopback interface address (localhost)
 PORT = 65431      # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -11,8 +11,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
-        conn.sendall(b"125,561,511,5518,51212,848,52121,51651")
+        conn.sendall(b"I MAKE ELEVATING MUSIC!")
         time.sleep(1)
-        # conn.send(b"FUCKER")
-        # time.sleep(1)
-        # conn.send(b"FULLER")
